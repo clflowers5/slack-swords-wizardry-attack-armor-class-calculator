29ac23d572d0b9e2a9b67ca9309027259d63d8c4
Chris Flowers <clflowers5@gmail.com>
2019-07-25 11:25:22 -0700

    add attack tables and randomness

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 
+import { CLERIC_DRUID_MONK_ATTACK_TABLE, FIGHTER_PALADIN_RANGER_ATTACK_TABLE } from './attackTables'
+// typings are incorrect for package
+const random = require('random')
+
 const app = express()
 const port = 5000
 
@@ -30,11 +34,25 @@ app.use(validateToken({ token: process.env.SLACK_VERIFICATION_TOKEN }))
 
 function parseInput (input) {
   // input format: [character-level] [bonus-to-hit]
-  const splitValues = input.split(/\s/)
+  const splitValues = input.split(/\s/).filter(val => val)
   if (!splitValues[0]) {
     throw new Error('Invalid Input, level is required')
   }
   return { level: splitValues[0], bonus: splitValues[1] || 0 }
+}
+
+function calculateHitArmorClass ({ level, attackRoll }) {
+  return {
+    clericDruidMonkArmorClass: 5,
+    fighterPaladinRangerArmorClass: 6
+  }
+}
+
+function buildResultText ({ clericDruidMonkArmorClass, fighterPaladinRangerArmorClass }) {
+  return `
+Cleric, Druid, Monk - You hit armor class: ${ clericDruidMonkArmorClass }
+Fighter, Paladin, Ranger - You hit armor class: ${ fighterPaladinRangerArmorClass }
+`
 }
 
 app.post('/v1/attack', (req, res) => {
@@ -47,11 +65,19 @@ app.post('/v1/attack', (req, res) => {
     res.json({ text: message })
   }
 
-  const hitArmorClass = 0
-  const attackRoll = 0
+  const attackRoll = random.int(1, 20)
+  const finalAttackRoll = Number(attackRoll) + Number(bonus)
+  const {
+    clericDruidMonkArmorClass,
+    fighterPaladinRangerArmorClass
+  } = calculateHitArmorClass({
+    level,
+    attackRoll: finalAttackRoll
+  })
+
   res.json({
     response_type: 'in_channel',
-    text: `You hit armor class: ${ hitArmorClass }`,
+    text: buildResultText({ clericDruidMonkArmorClass, fighterPaladinRangerArmorClass }),
     attachments: [
       {
         text: `1d20: ${ attackRoll }`,
